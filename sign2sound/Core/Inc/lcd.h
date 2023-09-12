921399e6c49d41938c7a7b0d94d54c0ad196d008
Kaushik Karthikeyan <karthik5@purdue.edu>
2023-09-12 11:01:26 -0400

    WRONG COE

---
+++ sign2sound/Core/Inc/lcd.h
@@ -0,0 +1,13 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Sep 8, 2023
+ *      Author: kaushik
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+
+
+#endif /* INC_LCD_H_ */
---
+++ sign2sound/Core/Src/lcd.c
@@ -0,0 +1,8 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Sep 8, 2023
+ *      Author: kaushik
+ */
+
+
